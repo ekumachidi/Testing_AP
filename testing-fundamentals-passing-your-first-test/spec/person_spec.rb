70dd7f6b1b1d05ae8d9a6cacbae4e689d1153da9
Ekuma Chidi <Bukki.arise@yahoo.co.uk>
2019-04-17 23:22:43 -0100

    Adds Passing Your First Test Solution

--- testing-fundamentals-passing-your-first-test/spec/person_spec.rb
+++ testing-fundamentals-passing-your-first-test/spec/person_spec.rb
@@ -3,9 +3,9 @@ require_relative "../lib/person"
 describe Person do
   describe "#full_name" do
     it "returns the first and last names concatenated" do
-      # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+      person = Person.new(first_name: "Jon", last_name: "Doe")
+
+      expect(person.full_name).to eq("Jon Doe")
     end
   end
 end
