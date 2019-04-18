3f6aeed1b4b4da5a067d3494d21cf3561a006d3d
Ekuma Chidi <Bukki.arise@yahoo.co.uk>
2019-04-17 23:22:43 -0100

    Adds Testing ActiveRecord Solution

--- testing-fundamentals-testing-activerecord/spec/models/person_spec.rb
+++ testing-fundamentals-testing-activerecord/spec/models/person_spec.rb
@@ -2,8 +2,14 @@ require "spec_helper"
 
 describe Person do
   it "requires a name to be valid" do
-    # Write your spec here.
-    # You can delete the pending line.
-    pending "Not written yet"
+    person = Person.new(first_name: "Jon")
+
+    expect(person).to be_valid
+  end
+
+  it "is invalid without a first_name" do
+    person = Person.new(first_name: nil)
+
+    expect(person).to be_invalid
   end
 end
